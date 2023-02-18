0911d2eb7c398d9e100f471e8ea7e0dd8db77966
Colepng <gamingwithcole7@gmail.com>
2023-02-18 15:52:16 -0500

    Add support for reloading waybar
    find a way to remove the time.sleep()

--- pywal/reload.py
+++ pywal/reload.py
@@ -5,6 +5,7 @@ import logging
 import os
 import shutil
 import subprocess
+import time
 
 from .settings import CACHE_DIR, MODULE_DIR, OS
 from . import util
@@ -76,6 +77,14 @@ def sway():
     if shutil.which("swaymsg") and util.get_pid("sway"):
         util.disown(["swaymsg", "reload"])
 
+def waybar():
+    "Reload waybar colors"
+    if shutil.which("waybar") and util.get_pid("waybar"):
+        util.disown(["killall", "waybar"])
+        time.sleep(0.001)
+        util.disown(shutil.which("waybar"))
+        # subprocess.Popen("waybar").pid()
+        # print(shutil.which("waybar"))
 
 def colors(cache_dir=CACHE_DIR):
     """Reload colors. (Deprecated)"""
@@ -96,5 +105,6 @@ def env(xrdb_file=None, tty_reload=True):
     kitty()
     sway()
     polybar()
+    waybar()
     logging.info("Reloaded environment.")
     tty(tty_reload)
